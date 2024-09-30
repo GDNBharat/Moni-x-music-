5971824f8ad0a18b49ccc8bc35ddc919ae2db039
GDNBharath <opchinna15@gmail.com>
2024-09-30 14:50:34 +0530

    Update and rename __init__.py to __init__.py

--- Dfschinnamusic/__init__.py
+++ Dfschinnamusic/__init__.py
@@ -1,8 +1,8 @@
-from AnonXMusic.core.bot import Anony
-from AnonXMusic.core.dir import dirr
-from AnonXMusic.core.git import git
-from AnonXMusic.core.userbot import Userbot
-from AnonXMusic.misc import dbb, heroku
+from Dfschinnamusic.core.bot import Dfs
+from Dfschinnamusic.core.dir import dirr
+from Dfschinnamusic.core.git import git
+from Dfschinnamusic.core.userbot import Userbot
+from Dfschinnamusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
@@ -11,7 +11,7 @@ dirr()
 dbb()
 heroku()
 
-app = Anony()
+app = Dfs()
 userbot = Userbot()
 
 
