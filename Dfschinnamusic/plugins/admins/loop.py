aed354442cde447f9888f64afa031c4da7d29bde
GDNBharath <opchinna15@gmail.com>
2024-09-30 16:39:34 +0530

    Update and rename loop.py to loop.py

--- Dfschinnamusic/plugins/admins/loop.py
+++ Dfschinnamusic/plugins/admins/loop.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_loop, set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from Dfschinnamusic import app
+from Dfschinnamusic.utils.database import get_loop, set_loop
+from Dfschinnamusic.utils.decorators import AdminRightsCheck
+from Dfschinnamusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
