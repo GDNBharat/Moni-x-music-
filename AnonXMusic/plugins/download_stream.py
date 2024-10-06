bd011bd70936b946f5755d060e06bb45512768cc
GDNBharath <opchinna15@gmail.com>
2024-10-06 14:07:54 +0530

    Update download_stream.py

--- AnonXMusic/plugins/download_stream.py
+++ AnonXMusic/plugins/download_stream.py
@@ -9,8 +9,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
 
-from AnonXMusic import app
-from AnonXMusic.platforms.Youtube import cookie_txt_file
+from Dfschinnamusic import app
+from Dfschinnamusic.platforms.Youtube import cookie_txt_file
 
 # Define a dictionary to track the last query timestamp for each user
 user_last_CallbackQuery_time = {}
