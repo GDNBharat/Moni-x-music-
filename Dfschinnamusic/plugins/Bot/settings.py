a6bfd37b439a51b7dd921834525f74c71926536d
GDNBharath <opchinna15@gmail.com>
2024-10-06 09:01:30 +0530

    Update settings.py

--- Dfschinnamusic/plugins/Bot/settings.py
+++ Dfschinnamusic/plugins/Bot/settings.py
@@ -9,7 +9,7 @@ from pyrogram.types import (
 )
 
 from Dfschinnamusic import app
-from AnonXMusic.utils.database import (
+from Dfschinnamusic.utils.database import (
     add_nonadmin_chat,
     get_authuser,
     get_authuser_names,
@@ -25,15 +25,15 @@ from AnonXMusic.utils.database import (
     skip_off,
     skip_on,
 )
-from AnonXMusic.utils.decorators.admins import ActualAdminCB
-from AnonXMusic.utils.decorators.language import language, languageCB
-from AnonXMusic.utils.inline.settings import (
+from Dfschinnamusic.utils.decorators.admins import ActualAdminCB
+from Dfschinnamusic.utils.decorators.language import language, languageCB
+from Dfschinnamusic.utils.inline.settings import (
     auth_users_markup,
     playmode_users_markup,
     setting_markup,
     vote_mode_markup,
 )
-from AnonXMusic.utils.inline.start import private_panel
+from Dfschinnamusic.utils.inline.start import private_panel
 from config import BANNED_USERS, OWNER_ID
 
 
