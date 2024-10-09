34155f7c46a7e3e91ca83247a40cc89bc9ec9e01
GDNBharath <opchinna15@gmail.com>
2024-10-09 11:37:07 +0530

    Update and rename blchat.py to blchat.py

--- Dfschinnamusic/plugins/sudo/blchat.py
+++ Dfschinnamusic/plugins/sudo/blchat.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
-from AnonXMusic.utils.decorators.language import language
+from Dfschinnamusic import app
+from Dfschinnamusic.misc import SUDOERS
+from Dfschinnamusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
+from Dfschinnamusic.utils.decorators.language import language
 from config import BANNED_USERS
 
 
