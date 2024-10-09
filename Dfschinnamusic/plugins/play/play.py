fca13117c8226612d3aa575c980758e64ba5c704
GDNBharath <opchinna15@gmail.com>
2024-10-09 09:37:27 +0530

    Update and rename play.py to play.py

--- Dfschinnamusic/plugins/play/play.py
+++ Dfschinnamusic/plugins/play/play.py
@@ -6,22 +6,22 @@ from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AnonXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils import seconds_to_min, time_to_seconds
-from AnonXMusic.utils.channelplay import get_channeplayCB
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.decorators.play import PlayWrapper
-from AnonXMusic.utils.formatters import formats
-from AnonXMusic.utils.inline import (
+from Dfschinnamusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from Dfschinnamusic.core.call import Dfs
+from Dfschinnamusic.utils import seconds_to_min, time_to_seconds
+from Dfschinnamusic.utils.channelplay import get_channeplayCB
+from Dfschinnamusic.utils.decorators.language import languageCB
+from Dfschinnamusic.utils.decorators.play import PlayWrapper
+from Dfschinnamusic.utils.formatters import formats
+from Dfschinnamusic.utils.inline import (
     botplaylist_markup,
     livestream_markup,
     playlist_markup,
     slider_markup,
     track_markup,
 )
-from AnonXMusic.utils.logger import play_logs
-from AnonXMusic.utils.stream.stream import stream
+from Dfschinnamusic.utils.logger import play_logs
+from Dfschinnamusic.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
 
