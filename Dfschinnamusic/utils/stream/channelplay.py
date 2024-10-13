b668431168c2fa0dcec6331d43a76cdd55d450dc
GDNBharat <opchinna742@gmail.com>
2024-10-13 16:00:42 +0530

    Update and rename AnonXMusic/utils/channelplay.py to Dfschinnamusic/utils/stream/channelplay.py

--- Dfschinnamusic/utils/stream/channelplay.py
+++ Dfschinnamusic/utils/stream/channelplay.py
@@ -1,5 +1,5 @@
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_cmode
+from Dfschinnamusic import app
+from Dfschinnamusic.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
