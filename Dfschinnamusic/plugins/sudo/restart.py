7e372d92c0b726ad565db402cb27409c1df722a8
Chinna op <opchinna742@gmail.com>
2024-10-20 20:53:53 +0530

    Update restart.py

--- Dfschinnamusic/plugins/sudo/restart.py
+++ Dfschinnamusic/plugins/sudo/restart.py
@@ -18,7 +18,7 @@ from Dfschinnamusic.utils.database import (
     remove_active_video_chat,
 )
 from Dfschinnamusic.utils.decorators.language import language
-from Dfschinnamusic.utils.pastebin import AnonyBin
+from Dfschinnamusic.utils.pastebin import Dfs
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
