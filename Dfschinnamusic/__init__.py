9397f703165fd1f969a1b183c72aacf620227148
Chinna op <opchinna742@gmail.com>
2024-11-07 12:56:56 +0530

    Update __init__.py

--- Dfschinnamusic/__init__.py
+++ Dfschinnamusic/__init__.py
@@ -6,22 +6,28 @@ from Dfschinnamusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
+# Directories
 dirr()
-#git()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
 dbb()
+
+# Heroku APP
 heroku()
 
-app = Dfs ()
+# Load Sudo Users from DB
+sudo()
+# Bot Client
+app = Dfs()
+
+# Assistant Client
 userbot = Userbot()
 
+from .platforms import PlaTForms
 
-from .platforms import *
-
-Apple = AppleAPI()
-Carbon = CarbonAPI()
-SoundCloud = SoundAPI()
-Spotify = SpotifyAPI()
-Resso = RessoAPI()
-Telegram = TeleAPI()
-YouTube = YouTubeAPI()
+Platform = PlaTForms()
+HELPABLE = {}
 
