508e040800f0cd42bda6e89cfc402132d7f0983e
Chinna op <opchinna742@gmail.com>
2024-11-08 06:45:36 +0530

    Update __init__.py

--- Dfschinnamusic/__init__.py
+++ Dfschinnamusic/__init__.py
@@ -1,1 +1,26 @@
+from Dfschinnamusic.core.bot import Dfs
+from Dfschinnamusic.core.dir import dirr
+from Dfschinnamusic.core.git import git
+from Dfschinnamusic.core.userbot import Userbot
+from Dfschinnamusic.misc import dbb, heroku
 
+from .logging import LOGGER
+
+dirr()
+#git()
+dbb()
+heroku()
+
+app = Dfs()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
