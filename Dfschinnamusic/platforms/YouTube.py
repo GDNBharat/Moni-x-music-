ec0cd7c956617f3b84949b63fca0e60abf2c44c2
Chinna op <opchinna742@gmail.com>
2024-11-12 07:05:53 +0530

    Update YouTube.py

--- Dfschinnamusic/platforms/YouTube.py
+++ Dfschinnamusic/platforms/YouTube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-
 from Dfschinnamusic.utils.formatters import time_to_seconds
+
 
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
