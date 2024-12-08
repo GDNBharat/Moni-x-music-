3e474206ec30ec55374138fa97089fa3b2444bae
Chinna op <opchinna742@gmail.com>
2024-12-08 12:36:58 +0530

    Update mustjoin.py

--- Dfschinnamusic/plugins/Bot/mustjoin.py
+++ Dfschinnamusic/plugins/Bot/mustjoin.py
@@ -47,7 +47,7 @@ async def must_join_channel(app: Client, msg: Message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/aboutBtschinna"),
+                                InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/monster_army_group"),
                                 InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=link),
                             ]
                         ]
