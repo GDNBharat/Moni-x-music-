54341bc539ebba51223e0bef90705916364bdd61
Chinna op <opchinna742@gmail.com>
2024-12-23 18:05:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = int(getenv("API_ID", "28362850"))
 API_HASH = getenv("API_HASH", "34f9cb93364db16fc45d003e4c81d97a")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "6829228356:AAEWdCRz6EI7z9j-MseWEVuKMXOQ2ptxfnI")
+BOT_TOKEN = getenv("BOT_TOKEN", "7660309192:AAEC_xFu6Zarq6bsGwkf_QzHT0t88wNz28M")
 # Get your MongoDB URI from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://VamsixD:VamsixD@vamsi.x7gyybv.mongodb.net/?retryWrites=true&w=majority")
 
