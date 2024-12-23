bb6b0ba545cb982f24202dcd617a11815d94003f
Chinna op <opchinna742@gmail.com>
2024-12-23 15:08:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = int(getenv("API_ID", "28362850"))
 API_HASH = getenv("API_HASH", "34f9cb93364db16fc45d003e4c81d97a")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "6829228356:AAEm7g43nrJ1Y932ET43sNzQzJncMSqE_Ik")
+BOT_TOKEN = getenv("BOT_TOKEN", "7734016901:AAGTb0wXU3eR60AlEu3OprQ6cn3HeLVglPY")
 # Get your MongoDB URI from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://VamsixD:VamsixD@vamsi.x7gyybv.mongodb.net/?retryWrites=true&w=majority")
 
