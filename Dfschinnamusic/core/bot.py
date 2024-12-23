33ddb8591c2a80dae35b1b6448da5f29b6cad864
Chinna op <opchinna742@gmail.com>
2024-12-23 14:41:43 +0530

    Update bot.py

--- Dfschinnamusic/core/bot.py
+++ Dfschinnamusic/core/bot.py
@@ -6,7 +6,7 @@ import config
 from ..logging import LOGGER
 
 
-class Dfs(Client):
+class DfsBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
